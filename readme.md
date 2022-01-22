8cf0f613d6c6428807722311a7a86b2219d7cc1e
Alex-Lont <alont@agl.com.au>
2022-01-22 11:43:31 +1100

    intial

--- readme.md
+++ readme.md
@@ -1,23 +1,3 @@
-Docker install for linux *note using ubuntu 20.04
-
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-
-get puid and pgid
-$ID
-
-#for hardware acceleration for plex *note using a quatro P400
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-sudo systemctl restart docker
-
 #for vpn apps qbit and jacket
 vpn files in config
 /config/openvpn
---
+++ run.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#read .evn file
+set -o xtrace
+export $(egrep -v '^#' .env | xargs)
+
+apt install -y net-tools 
+#set static ip
+sed -r 's/^(\s*)(addresses\s*:\s*nginx\s*$)/\1image: apache/' file
+
+cp ./01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
+netplan apply
+ifconfig
+
+#Docker install for linux *note using ubuntu 20.04
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
+
+#for hardware acceleration for plex *note using a quatro P400
+#distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update && apt-get install -y nvidia-container-toolkit
+systemctl restart docker
+
+#ip link add ${DOCKER_ROUTING_INTERFACE_NAME} link ${network_driver} type macvlan mode bridge 
+#ip addr add ${DOCKERNETWORK_IP_ADDRESS} dev ${DOCKER_ROUTING_INTERFACE_NAME} 
+#ip link set ${DOCKER_ROUTING_INTERFACE_NAME} up
+#ip route add ${DOCKERNETWORK_IP_RANGE} dev ${DOCKER_ROUTING_INTERFACE_NAME}
